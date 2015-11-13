1a559665bc48a1de1ca628c1ef0145e8febac648
abay <abaywashere@gmail.com>
2015-11-14 00:03:43 +0700

    mentok di bagian login with fb reference http://code.tutsplus.com/tutorials/quick-tip-add-facebook-login-to-your-android-app--cms-23837

--- app/src/main/java/makasa/dapurkonten/jodohideal/Login.java
+++ app/src/main/java/makasa/dapurkonten/jodohideal/Login.java
@@ -8,6 +8,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 public class Login extends AppCompatActivity {
 
     @Override
@@ -25,6 +31,30 @@ public class Login extends AppCompatActivity {
     public void register (View view){
         Intent i = new Intent(Login.this, Register.class);
         startActivity(i);
+    }
+    private CallbackManager callbackManager;
+    private LoginButton loginButton;
+
+    public void loginwithfb (View view){
+        callbackManager = CallbackManager.Factory.create();
+        setContentView(R.layout.activity_login);
+        loginButton = (LoginButton)findViewById(R.id.btnLoginFacebook);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+
+            }
+        });
     }
 
 }
