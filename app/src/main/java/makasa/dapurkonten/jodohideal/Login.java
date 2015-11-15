6b3057244d12d65d480dcabb866357c12d6b2156
abay <abaywashere@gmail.com>
2015-11-15 11:52:10 +0700

    ubah login fb button

--- app/src/main/java/makasa/dapurkonten/jodohideal/Login.java
+++ app/src/main/java/makasa/dapurkonten/jodohideal/Login.java
@@ -1,16 +1,22 @@
 package makasa.dapurkonten.jodohideal;
 
+import com.facebook.FacebookSdk;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
@@ -19,6 +25,7 @@ public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_login);
 
     }
@@ -32,17 +39,20 @@ public class Login extends AppCompatActivity {
         Intent i = new Intent(Login.this, Register.class);
         startActivity(i);
     }
-    private CallbackManager callbackManager;
-    private LoginButton loginButton;
+    public class MainFragment extends Fragment {
 
-    public void loginwithfb (View view){
-        callbackManager = CallbackManager.Factory.create();
-        setContentView(R.layout.activity_login);
-        loginButton = (LoginButton)findViewById(R.id.btnLoginFacebook);
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        private CallbackManager callbackManager;
+        private TextView textView;
+
+        private AccessTokenTracker accessTokenTracker;
+        private ProfileTracker profileTracker;
+
+        private FacebookCallback<LoginResult> callback = new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-
+                AccessToken accessToken = loginResult.getAccessToken();
+                Profile profile = Profile.getCurrentProfile();
+                displayMessage(profile);
             }
 
             @Override
@@ -54,7 +64,83 @@ public class Login extends AppCompatActivity {
             public void onError(FacebookException e) {
 
             }
-        });
+        };
+
+        public MainFragment() {
+
+        }
+
+
+        @Override
+        public void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
+
+            callbackManager = CallbackManager.Factory.create();
+
+            accessTokenTracker= new AccessTokenTracker() {
+                @Override
+                protected void onCurrentAccessTokenChanged(AccessToken oldToken, AccessToken newToken) {
+
+                }
+            };
+
+            profileTracker = new ProfileTracker() {
+                @Override
+                protected void onCurrentProfileChanged(Profile oldProfile, Profile newProfile) {
+                    displayMessage(newProfile);
+                }
+            };
+
+            accessTokenTracker.startTracking();
+            profileTracker.startTracking();
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.activity_login, container, false);
+        }
+
+        @Override
+        public void onViewCreated(View view, Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            LoginButton loginButton = (LoginButton) view.findViewById(R.id.login_button);
+            textView = (TextView) view.findViewById(R.id.textView);
+
+            loginButton.setReadPermissions("user_friends");
+            loginButton.setFragment(this);
+            loginButton.registerCallback(callbackManager, callback);
+
+        }
+
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            callbackManager.onActivityResult(requestCode, resultCode, data);
+
+        }
+
+        private void displayMessage(Profile profile){
+            if(profile != null){
+                textView.setText(profile.getName());
+            }
+        }
+
+        @Override
+        public void onStop() {
+            super.onStop();
+            accessTokenTracker.stopTracking();
+            profileTracker.stopTracking();
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            Profile profile = Profile.getCurrentProfile();
+            displayMessage(profile);
+        }
     }
+
 
 }
