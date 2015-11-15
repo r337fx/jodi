adb042d68409d6eac8ff772c8df3a65e07b7ce37
abay <abaywashere@gmail.com>
2015-11-15 16:03:41 +0700

    login api

--- app/src/main/java/makasa/dapurkonten/jodohideal/Login.java
+++ app/src/main/java/makasa/dapurkonten/jodohideal/Login.java
@@ -1,146 +1,93 @@
 package makasa.dapurkonten.jodohideal;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.facebook.FacebookSdk;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import com.facebook.AccessToken;
-import com.facebook.AccessTokenTracker;
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.Profile;
-import com.facebook.ProfileTracker;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Login extends AppCompatActivity {
+    private static final String REGISTER_URL = "http://rupiadam.net/api/test.php";
 
+    public static final String KEY_USERNAME = "username";
+    public static final String KEY_PASSWORD = "password";
+
+
+    private EditText editTextUsername;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+
+    private Button buttonRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_login);
+        editTextUsername = (EditText) findViewById(R.id.email);
+        editTextPassword = (EditText) findViewById(R.id.password);
+
+        buttonRegister = (Button) findViewById(R.id.btnLogin);
 
     }
 
-    public void login (View view){
-        Intent i = new Intent(Login.this, MainActivity.class);
-        startActivity(i);
-    }
+
 
     public void register (View view){
         Intent i = new Intent(Login.this, Register.class);
         startActivity(i);
     }
-    public class MainFragment extends Fragment {
+    private void registerUser(){
+        final String email = editTextUsername.getText().toString().trim();
+        final String password = editTextPassword.getText().toString().trim();
 
-        private CallbackManager callbackManager;
-        private TextView textView;
-
-        private AccessTokenTracker accessTokenTracker;
-        private ProfileTracker profileTracker;
-
-        private FacebookCallback<LoginResult> callback = new FacebookCallback<LoginResult>() {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, REGISTER_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        //Toast.makeText(Login.this, response, Toast.LENGTH_LONG).show();
+                        if(response.equals("1")){
+                            Toast.makeText(Login.this, "Success ", Toast.LENGTH_LONG).show();
+                        }
+                        else{
+                            Toast.makeText(Login.this, "Not Success", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(Login.this,error.toString(),Toast.LENGTH_LONG).show();
+                    }
+                }){
             @Override
-            public void onSuccess(LoginResult loginResult) {
-                AccessToken accessToken = loginResult.getAccessToken();
-                Profile profile = Profile.getCurrentProfile();
-                displayMessage(profile);
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put(KEY_USERNAME,email);
+                params.put(KEY_PASSWORD,password);
+                return params;
             }
 
-            @Override
-            public void onCancel() {
-
-            }
-
-            @Override
-            public void onError(FacebookException e) {
-
-            }
         };
 
-        public MainFragment() {
-
-        }
-
-
-        @Override
-        public void onCreate(Bundle savedInstanceState){
-            super.onCreate(savedInstanceState);
-            FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
-
-            callbackManager = CallbackManager.Factory.create();
-
-            accessTokenTracker= new AccessTokenTracker() {
-                @Override
-                protected void onCurrentAccessTokenChanged(AccessToken oldToken, AccessToken newToken) {
-
-                }
-            };
-
-            profileTracker = new ProfileTracker() {
-                @Override
-                protected void onCurrentProfileChanged(Profile oldProfile, Profile newProfile) {
-                    displayMessage(newProfile);
-                }
-            };
-
-            accessTokenTracker.startTracking();
-            profileTracker.startTracking();
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.activity_login, container, false);
-        }
-
-        @Override
-        public void onViewCreated(View view, Bundle savedInstanceState) {
-            super.onViewCreated(view, savedInstanceState);
-            LoginButton loginButton = (LoginButton) view.findViewById(R.id.login_button);
-            textView = (TextView) view.findViewById(R.id.textView);
-
-            loginButton.setReadPermissions("user_friends");
-            loginButton.setFragment(this);
-            loginButton.registerCallback(callbackManager, callback);
-
-        }
-
-        @Override
-        public void onActivityResult(int requestCode, int resultCode, Intent data) {
-            super.onActivityResult(requestCode, resultCode, data);
-            callbackManager.onActivityResult(requestCode, resultCode, data);
-
-        }
-
-        private void displayMessage(Profile profile){
-            if(profile != null){
-                textView.setText(profile.getName());
-            }
-        }
-
-        @Override
-        public void onStop() {
-            super.onStop();
-            accessTokenTracker.stopTracking();
-            profileTracker.stopTracking();
-        }
-
-        @Override
-        public void onResume() {
-            super.onResume();
-            Profile profile = Profile.getCurrentProfile();
-            displayMessage(profile);
-        }
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 
+    public void login (View view){
+        registerUser();
+    }
 
 }
