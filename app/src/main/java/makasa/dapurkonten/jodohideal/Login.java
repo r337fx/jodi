2a86031eeed55c0236d5691f8b7fc732e15769a8
abay <abaywashere@gmail.com>
2015-11-16 14:00:55 +0700

    add fitur login as facebook

--- app/src/main/java/makasa/dapurkonten/jodohideal/Login.java
+++ app/src/main/java/makasa/dapurkonten/jodohideal/Login.java
@@ -6,15 +6,24 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,7 +33,9 @@ public class Login extends AppCompatActivity {
 
     public static final String KEY_USERNAME = "username";
     public static final String KEY_PASSWORD = "password";
-
+    private TextView info;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
 
     private EditText editTextUsername;
     private EditText editTextPassword;
@@ -34,15 +45,41 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.activity_login);
+        info = (TextView)findViewById(R.id.message);
+        loginButton = (LoginButton)findViewById(R.id.login_button);
         editTextUsername = (EditText) findViewById(R.id.email);
         editTextPassword = (EditText) findViewById(R.id.password);
 
         buttonRegister = (Button) findViewById(R.id.btnLogin);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Profile profile = Profile.getCurrentProfile();
+                info.setText(
+                        "User ID: "
+                                + profile.getId()+
+                        "Nama:"+ profile.getName()
+                );
+            }
+
+            @Override
+            public void onCancel() {
+                info.setText("Login attempt canceled.");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                info.setText("Login attempt failed.");
+            }
+        });
 
     }
 
-
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 
     public void register (View view){
         Intent i = new Intent(Login.this, Register.class);
