d146cc3697903397665c1a8acf292a1a816486b7
abay <abaywashere@gmail.com>
2015-12-12 22:25:13 +0700

    relayouting and styling question activity

--- app/src/main/java/makasa/dapurkonten/jodohideal/questionsActivity.java
+++ app/src/main/java/makasa/dapurkonten/jodohideal/questionsActivity.java
@@ -17,8 +17,8 @@ import java.util.HashMap;
 
 public class questionsActivity extends AppCompatActivity{
     private SQLiteController db;
-    TextView pertanyaans;
-    Button next;
+    TextView pertanyaans,idquestion;
+    Button goto_next,goto_prev;
     RadioGroup groupQuestion;
     RadioButton question1,question2;
     int idpertanyaan;
@@ -31,11 +31,13 @@ public class questionsActivity extends AppCompatActivity{
         idpertanyaan = 1;
         setContentView(R.layout.activity_question);
         pertanyaans = (TextView)findViewById(R.id.questiont);
-        next = (Button)findViewById(R.id.goto_next);
+        idquestion = (TextView)findViewById(R.id.idQuestiont);
+        goto_next = (Button)findViewById(R.id.goto_next);
+        goto_prev = (Button)findViewById(R.id.goto_previous);
         groupQuestion = (RadioGroup)findViewById(R.id.groupQuestion);
         question1 = (RadioButton)findViewById(R.id.question1);
         question2 = (RadioButton)findViewById(R.id.question2);
-
+        goto_prev.setVisibility(View.INVISIBLE);
         db = new SQLiteController(getApplicationContext());
         HashMap<String,String> tenQuestion = db.getIdQuestion(idpertanyaan);
         String id=tenQuestion.get("id"),
@@ -44,12 +46,19 @@ public class questionsActivity extends AppCompatActivity{
                 answer_ops1=tenQuestion.get("answer_ops1"),
                 answer_ops2=tenQuestion.get("answer_ops2");
         pertanyaans.setText(question);
+        idquestion.setText(id);
         question1.setText(answer_ops1);
         question2.setText(answer_ops2);
+
 
     }
     public void next (View view){
         if(idpertanyaan<10) {
+            idpertanyaan++;
+            groupQuestion.clearCheck();
+            if(idpertanyaan==10){
+                goto_next.setText("Finish");
+            }
             HashMap<String,String> tenQuestion = db.getIdQuestion(idpertanyaan);
             String id=tenQuestion.get("id"),
                     question_id=tenQuestion.get("question_id"),
@@ -57,29 +66,19 @@ public class questionsActivity extends AppCompatActivity{
                     answer_ops1=tenQuestion.get("answer_ops1"),
                     answer_ops2=tenQuestion.get("answer_ops2");
             pertanyaans.setText(question);
+            idquestion.setText(id);
             question1.setText(answer_ops1);
             question2.setText(answer_ops2);
-
-            /**
-             * lol ribet eamng bacanya
-             * jadi supaya dapet tipe data bool
-             * lu harus panggil fungsi indexOfChild(findViewByID(R.id.xxx))
-             * terus definisiin apa id radio buttonnya,
-             * karena disini dinamis, kita buat idnya itu pake getCheckedButtonID()
-             * tested works
-             */
-            int selectedID = groupQuestion.indexOfChild(findViewById(groupQuestion.getCheckedRadioButtonId()));
-
-            question1.setChecked(false);
-            question2.setChecked(false);
-            idpertanyaan++;
+            goto_prev.setVisibility(View.VISIBLE);
         }
     }
     public void previous (View view){
         if(idpertanyaan>1) {
-            question1.setChecked(false);
-            question2.setChecked(false);
             idpertanyaan--;
+            if(idpertanyaan==1){
+                goto_prev.setVisibility(View.INVISIBLE);
+            }
+            groupQuestion.clearCheck();
             HashMap<String,String> tenQuestion = db.getIdQuestion(idpertanyaan);
             String id=tenQuestion.get("id"),
                     question_id=tenQuestion.get("question_id"),
@@ -87,8 +86,11 @@ public class questionsActivity extends AppCompatActivity{
                     answer_ops1=tenQuestion.get("answer_ops1"),
                     answer_ops2=tenQuestion.get("answer_ops2");
             pertanyaans.setText(question);
+            idquestion.setText(id);
             question1.setText(answer_ops1);
             question2.setText(answer_ops2);
+            goto_next.setText("Next");
+
         }
     }
 }
